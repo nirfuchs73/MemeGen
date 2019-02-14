be8ec0f9cae8456e4dbdda36c0cfd2e64aa3a1cf
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-14 17:38:58 +0200

    Fix merge

--- js/memes-main.js
+++ js/memes-main.js
@@ -1,44 +1,66 @@
 function init() {
-  createImages()
-  renderGallery();
+    createImages()
+    renderGallery();
 }
 
 function renderGallery() {
-  var images = getImagesForDisplay()
-  var strHtmls = images.map(function (image) {
-      return `
+    var images = getImagesForDisplay()
+    var strHtmls = images.map(function (image) {
+        return `
     <div class="img img-1" style="background-image: url(${image.url}); background-repeat: no-repeat; background-size: cover; background-position: center center; position: relative;
-     width:100%; object-fit: cover"></div>`
-  })
-  //console.log(strHtmls.join(''))
-  var container = document.querySelector('.main-container');
-  container.innerHTML = strHtmls.join('')
+     width:100%; object-fit: cover" onclick="onImageClicked(${image.id})"></div>`
+    })
+    //console.log(strHtmls.join(''))
+    var container = document.querySelector('.main-container');
+    container.innerHTML = strHtmls.join('')
 
 }
+
+function onImageClicked(id) {
+    elGallery = document.querySelector('.gallery-main');
+    elGallery.classList.add('hide');
+
+    elCanvas = document.querySelector('.canvas-main-container');
+    elCanvas.classList.remove('hide');
+    initCanvas(id);
+}
+
+function onBacktoGallery() {
+    elGallery = document.querySelector('.gallery-main');
+    elGallery.classList.remove('hide');
+
+    elCanvas = document.querySelector('.canvas-main-container');
+    elCanvas.classList.add('hide');
+
+    init();
+}
+
 console.log(renderFirstFilter())
- function renderFirstFilter(){
-//take the unique valuies from the image array on select send key param
-var keywords=[];
-var allMemes=getImagesForDisplay();
-console.log('meme', allMemes)
-var filtered= allMemes.filter(function (meme) {
-  if(!keywords[meme.keywords]){push.keywords[meme.keywords] 
-   return  true}
- })
-console.log(keywords)
-return keywords
-console.log(kewords)
- }
-
-function onSelectFilter( keyWord){   
-//finds the object by key name makes the new array for display
+function renderFirstFilter() {
+    //take the unique valuies from the image array on select send key param
+    var keywords = [];
+    var allMemes = getImagesForDisplay();
+    console.log('meme', allMemes)
+    var filtered = allMemes.filter(function (meme) {
+        if (!keywords[meme.keywords]) {
+            push.keywords[meme.keywords]
+            return true
+        }
+    })
+    console.log(keywords)
+    return keywords
+    console.log(kewords)
 }
 
-function renderSecndFilter(){
-// array of popularyty {keyword: numof times serched}
-//the size will be function of poularity 
+function onSelectFilter(keyWord) {
+    //finds the object by key name makes the new array for display
+}
 
-//
+function renderSecndFilter() {
+    // array of popularyty {keyword: numof times serched}
+    //the size will be function of poularity 
+
+    //
 }
 'use strict';
 var gCanvas;
@@ -46,7 +68,6 @@ var gCtx;
 // var gColor;
 // var gImage;
 var gImageObj;
-var g
 
 function initCanvas(id) {
     createImages();
